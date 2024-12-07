e4a467636179c65276442e565882d4061a8fcaf3
rnujella <rnujella@binghamton.edu>
2024-12-07 16:21:54 -0500

    Update ast.hs

--- ast/ast.hs
+++ ast/ast.hs
@@ -1,16 +1,23 @@
 module Ast where
 
--- Define the expression data type
-data Expr = Plus Expr Expr 
-          | Minus Expr Expr 
-          | Times Expr Expr 
-          | Div Expr Expr
-          | Literal Float
+-- Define the Expr data type
+data Expr 
+    = Plus Expr Expr 
+    | Minus Expr Expr 
+    | Times Expr Expr 
+    | Div Expr Expr
+    | Literal Float
+    deriving (Show, Eq) -- Add Show and Eq for debugging and comparisons
 
--- Implement the eval function
+-- Define the eval function to evaluate expressions
 eval :: Expr -> Float
 eval (Plus e1 e2) = eval e1 + eval e2
 eval (Minus e1 e2) = eval e1 - eval e2
 eval (Times e1 e2) = eval e1 * eval e2
 eval (Div e1 e2) = eval e1 / eval e2
 eval (Literal n) = n
+
+-- Test expressions (for internal testing/debugging)
+test1 = Plus (Literal 3.0) (Literal 2.0)
+test2 = Plus (Literal 3.0) (Div (Literal 1.0) (Literal 2.0))
+test3 = Plus (Times (Literal 3.0) (Literal 5.0)) (Div (Literal 1.0) (Literal 2.0))
